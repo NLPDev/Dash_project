5a07cd83c4a94d05df2372e6505fcf88f55eb6b7
NLPDev <vasile123andronic@gmail.com>
2020-04-02 18:39:58 -0700

    show table data

--- dash_example.py
+++ dash_example.py
@@ -1,6 +1,21 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -11,39 +26,44 @@ colors = {
     'text': '#7FDBFF'
 }
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Hello Dash',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
-
-    html.Div(children='Dash: A web application framework for Python.',
-             style={
-                 'textAlign': 'center',
-                 'color': colors['text']
-             }
-    ),
-
-    dcc.Graph(
-        id='example-graph-2',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 15], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montreal'},
-            ],
-            'layout': {
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                }
-            }
-        }
-    )
+app.layout = html.Div(children=[
+    html.H4(children='US Agriculture Experts (2011'),
+    generate_table(df)
 ])
+
+# app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+#     html.H1(
+#         children='Hello Dash',
+#         style={
+#             'textAlign': 'center',
+#             'color': colors['text']
+#         }
+#     ),
+#
+#     html.Div(children='Dash: A web application framework for Python.',
+#              style={
+#                  'textAlign': 'center',
+#                  'color': colors['text']
+#              }
+#     ),
+#
+#     dcc.Graph(
+#         id='example-graph-2',
+#         figure={
+#             'data': [
+#                 {'x': [1, 2, 3], 'y': [4, 1, 15], 'type': 'bar', 'name': 'SF'},
+#                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montreal'},
+#             ],
+#             'layout': {
+#                 'plot_bgcolor': colors['background'],
+#                 'paper_bgcolor': colors['background'],
+#                 'font': {
+#                     'color': colors['text']
+#                 }
+#             }
+#         }
+#     )
+# ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
